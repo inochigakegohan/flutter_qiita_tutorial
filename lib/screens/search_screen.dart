62cee425ee6d9ce28c37083a4a266a92916105e2
janglemojamoja <kishiura6202@gmail.com>
2024-05-27 09:42:52 +0900

    feat: QiitaSearch画面を作成

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -18,23 +18,3 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 }
-import 'package:flutter/material.dart';
-
-class SearchScreen extends StatefulWidget {
-  const SearchScreen({super.key});
-
-  @override
-  State<SearchScreen> createState() => _SearchScreenState();
-}
-
-class _SearchScreenState extends State<SearchScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Qiita Search'),
-      ),
-      body: Container(),
-    );
-  }
-}
