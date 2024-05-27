6e83a38431a08aa5e4ed76b1c231dd22f5dbe928
janglemojamoja <kishiura6202@gmail.com>
2024-05-28 08:46:03 +0900

    style: コード整形

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,19 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Qiita Search', // titleを追加
       theme: ThemeData( // themeを追加
-      primarySwatch: Colors.green,
-      fontFamily: 'Hiragino Sans',
-      appBarTheme: const AppBarTheme(
-      backgroundColor: Color(0xFF55C500),
-    ),
-    textTheme: Theme.of(context).textTheme.apply(
-    bodyColor: Colors.white,
-    ),
-    ),
-    home: const SearchScreen(), // SearchScreenを設定
+        primarySwatch: Colors.green,
+        fontFamily: 'Hiragino Sans',
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF55C500),
+        ),
+        textTheme: Theme
+            .of(context)
+            .textTheme
+            .apply(
+          bodyColor: Colors.white,
+        ),
+      ),
+      home: const SearchScreen(), // SearchScreenを設定
     );
   }
 }
