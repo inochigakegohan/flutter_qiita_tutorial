52167c3feede25e78f2ed23eb632f31d7bb85c31
janglemojamoja <kishiura6202@gmail.com>
2024-05-28 09:13:46 +0900

    feat: factoryコンストラクタを定義

--- lib/models/article.dart
+++ lib/models/article.dart
@@ -1,3 +1,5 @@
+import 'package:qiita_search/models/user.dart';
+
 class Article {
   // コンストラクタ
   Article({
@@ -16,4 +18,19 @@ class Article {
   final List<String> tags;
   final DateTime createdAt;
   final String url;
+
+  // JSONからArticleを生成するファクトリコンストラクタ
+  factory Article.fromJson(dynamic json) {
+    return Article(
+      title: json['title'] as String,
+      user: User.fromJson(json['user']),
+      // User.fromJson()を使ってUserを生成
+      url: json['url'] as String,
+      createdAt: DateTime.parse(json['created_at'] as String),
+      // DateTime.parse()を使って文字列をDateTimeに変換
+      likesCount: json['likes_count'] as int,
+      tags: List<String>.from(json['tags'].map((
+          tag) => tag['name'])), // List<String>.from()を使ってList<String>に変換
+    );
+  }
 }
