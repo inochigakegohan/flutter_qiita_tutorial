e0a02385d59bbb3360697b534f4d0af2ab6a034b
janglemojamoja <kishiura6202@gmail.com>
2024-05-30 09:42:46 +0900

    feat: 検索ボックスの完成

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -12,6 +12,7 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  List<Article> articles = []; // 検索結果を格納する変数
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,8 +37,9 @@ class _SearchScreenState extends State<SearchScreen> {
                 // ← InputDecorationを渡す
                 hintText: '検索ワードを入力してください',
               ),
-              onSubmitted: (String value) {
-                print("検索中: $value");
+              onSubmitted: (String value) async {
+                final results = await searchQiita(value); //検索処理を実行す
+                setState(()=> articles = results); //検索結果を代入
               },
             ),
           ),
